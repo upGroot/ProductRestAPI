50ad19753fb90b80acab874598d5de0b8348aeb5
upGroot <ipathakayush@gmail.com>
2022-05-14 22:59:17 +0530

    Logging Added

--- src/main/java/com/example/productrestapi/ProductRestController.java
+++ src/main/java/com/example/productrestapi/ProductRestController.java
@@ -1,7 +1,7 @@
 package com.example.productrestapi;
 
-import com.example.productrestapi.Product;
-import com.example.productrestapi.ProductRespository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +10,7 @@ import java.util.List;
 @RestController
 public class ProductRestController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProductRestController.class);
     @Autowired
     ProductRespository productRespository;
 
@@ -20,6 +21,7 @@ public class ProductRestController {
 
     @RequestMapping(value = "/product/{id}", method = RequestMethod.GET)
     public Product getProduct(@PathVariable(value = "id") int id){
+        LOGGER.info("Find product by ID:"+id);
         return productRespository.findById(id).get();
     }
 
--- src/test/java/com/example/productrestapi/ProductRestApiApplicationTests.java
+++ src/test/java/com/example/productrestapi/ProductRestApiApplicationTests.java
@@ -2,12 +2,30 @@ package com.example.productrestapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
+import static org.junit.Assert.*;
 
 @SpringBootTest
 class ProductRestApiApplicationTests {
 
     @Test
     void contextLoads() {
+        RestTemplate restTemplate = new RestTemplate();
+        Product product = restTemplate.getForObject("http://localhost:8080/productapi/product/1", Product.class);
+        assertNotNull(product);
+        assertEquals("Iphone", product.getName());
+    }
+
+    @Test
+    void PostProduct(){
+        RestTemplate restTemplate =  new RestTemplate();
+        Product product = new Product();
+        product.setName("Samsung Mobile");
+        product.setDescription("Worst Mobile ever made");
+        product.setPrice(2.0);
+        Product newProduct = restTemplate.postForObject("http://localhost:8080/productapi/product/", product, Product.class);
+        assertNotNull(newProduct);
+        assertEquals("Samsung Mobile", newProduct.getName());
     }
 
 }
